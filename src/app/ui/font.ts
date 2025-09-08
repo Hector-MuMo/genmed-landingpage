85f41a47b5256833c9da3103a574dbe371837d4d
Hector Muñoz <hectormm.wrk@gmail.com>
2025-09-08 11:48:22 -0600

    feat: create configs. eslint and heroui

---
+++ src/app/ui/font.ts
@@ -0,0 +1,11 @@
+import { Roboto_Slab, Oregano } from "next/font/google";
+
+export const roboto_slab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["200", "400", "600", "800"],
+});
+
+export const oregano = Oregano({
+  weight: ["400"],
+  subsets: ["latin"],
+});

de725ee8f3a3c977d1d1f58b8f27b898fa77c794
Hector Muñoz <hectormm.wrk@gmail.com>
2025-09-11 11:24:54 -0600

    feat: nostros page responsive

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -5,11 +5,44 @@ const Nosotros = () => {
   return (
     <div className="bg-[url(../../public/Heroimg-us.png)] bg-cover sm:bg-none min-h-dvh p-2 pb-2">
       <main>
-        <h1 className={`${oregano.className} text-6xl text-amber-50`}>
-          Nosotros
-        </h1>
-        <div className="bg-white/70 rounded-lg mt-15 py-5 px-3">
-          <h2 className="mb-8 text-2xl text-center text-stone-600">Más de [X] Años Creando Sonrisas</h2>
+        <div className="sm:hidden">
+          <h1
+            className={`${oregano.className} text-6xl text-amber-50 sm:text-stone-800`}
+          >
+            Nosotros
+          </h1>
+          <div className="bg-white/70 rounded-lg mt-15 py-5 px-3">
+            <h2 className="mb-8 text-2xl text-center text-stone-600">
+              Más de [X] Años Creando Sonrisas
+            </h2>
+            <p className="text-stone-900 text-justify">
+              Lorem ipsum dolor sit amet consectetur. Gravida nunc in porttitor
+              ut mauris elit nibh. Netus viverra adipiscing mus dui. Luctus eget
+              mauris gravida nibh elementum risus. Nunc bibendum eu potenti
+              iaculis duis amet nunc. Natoque euismod ullamcorper sit aliquet
+              mauris. Hendrerit mauris scelerisque risus eget. Viverra ut
+              egestas dictum aliquet laoreet pulvinar nunc ultrices dictum.
+              Lobortis lorem et vitae lobortis tortor fermentum eget eu. Natoque
+              aenean aliquam enim orci tincidunt id habitasse id ut. Tempor
+              porta leo fusce viverra tortor tristique integer ultricies nisl.
+              Eleifend amet euismod cursus quis ut nisi egestas.
+            </p>
+          </div>
+        </div>
+        <div className="hidden sm:grid sm:grid-cols-3">
+          <div className="flex justify-center items-center">
+          <h2 className="mb-8 sm:text-4xl lg:text-5xl text-center text-stone-600 sm:px-5">
+            Más de [X] Años Creando Sonrisas
+          </h2>
+          </div>
+          <div className="sm:bg-[url(../../public/Heroimg-us.png)] sm:bg-cover bg-center sm:h-svh">
+            <h1
+              className={`${oregano.className} text-6xl text-amber-50 sm:text-stone-800 text-center`}
+            >
+              Nosotros
+            </h1>
+          </div>
+          <div className="sm:flex sm:justify-center sm:items-center sm:text-md sm:px-5">
           <p className="text-stone-900 text-justify">
             Lorem ipsum dolor sit amet consectetur. Gravida nunc in porttitor ut
             mauris elit nibh. Netus viverra adipiscing mus dui. Luctus eget
@@ -22,6 +55,7 @@ const Nosotros = () => {
             viverra tortor tristique integer ultricies nisl. Eleifend amet
             euismod cursus quis ut nisi egestas.
           </p>
+          </div>
         </div>
       </main>
     </div>

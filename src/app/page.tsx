c9b9dbbf82a2cf39de5ecd3ab6b08385d51e6c23
Hector Muñoz <hectormm.wrk@gmail.com>
2025-09-11 11:11:49 -0600

    feat: home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-[url(../../public/Heroimg-home.png)] bg-cover sm:bg-none min-h-dvh p-2 pb-2">
-      <main>
-        <div>
-          <h1 className={`${oregano.className} text-6xl text-amber-50`}>
-            Odontología <span className="text-stone-600">de Primera</span> para
+      <main className="sm:flex sm:flex-col sm:justify-center sm:items-center">
+        <div className="sm:flex">
+          <div className="sm:w-1/2 sm:h-svh sm:flex sm:flex-col sm:items-center sm:justify-center">
+          <h1 className={`${oregano.className} text-6xl text-amber-50 sm:text-stone-400 sm:text-center`}>
+            Odontología <span className="text-stone-600 sm:text-stone-900">de Primera</span> para
             toda la Familia.
           </h1>
           <div className="flex justify-center items-center py-5">
@@ -22,12 +23,13 @@ export default function Home() {
               Conocenos
             </Button>
           </div>
+          </div>
+          <div className="sm:bg-[url(../../public/Heroimg-home.png)] sm:bg-cover sm:bg-no-repeat bg-center sm:w-1/2 sm:h-smvh lg:w-[800px]"></div>
         </div>
-        <div className="h-96"></div>
-        <p className="bg-white/30 rounded-lg text-amber-50 text-lg text-center">
+        <p className="bg-white/30 rounded-lg max-w-1/2 text-amber-50 text-lg text-center sm:text-stone-400">
           Transformamos sonrisas y mejoramos tu{" "}
-          <span className="text-stone-600">salud</span> bucodental con los más
-          altos estándares de <span className="text-stone-600">calidad</span>.
+          <span className="text-stone-600 font-bold sm:text-stone-900">salud</span> bucodental con los más
+          altos estándares <span className="text-stone-600 font-bold sm:text-stone-900">de calidad</span>.
         </p>
       </main>
     </div>
